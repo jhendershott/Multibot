8b4c5c4c3377bfa54e51a4c43ed008ad173cefc9
Nyasaki <walk.marcel@nyasaki.dev>
2020-07-02 00:57:24 +0200

    Added C# folder and .NET Core base

---
+++ C#/multicorp-bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multicorp_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
