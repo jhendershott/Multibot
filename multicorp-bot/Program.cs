82d0fca827e40a34e18a4e7b19651ce3672713da
joelH <joel.hendershott@gmail.com>
2021-02-09 09:20:28 -0600

    removing auto reply functions

--- multicorp-bot/Program.cs
+++ multicorp-bot/Program.cs
@@ -37,41 +37,41 @@ namespace multicorp_bot {
 
             var command = new Commands();
 
-            await Task.Run(() => discord.MessageCreated += Discord_MessageCreated);
+            //await Task.Run(() => discord.MessageCreated += Discord_MessageCreated);
 
             await discord.ConnectAsync ();
             await Task.Delay (-1);
         }
 
-        private static async Task Discord_MessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs e)
-        {
-            var command = new Commands();
-            string[] messageStrings = new string[] { "bot", "multibot" };
-            var strArray = e.Message.Content.Split(" ");
-            string[] prohibChannelArr = new string[]
-            {
-                "command-chat",
-                "officer-quarters",
-                "op-planning",
-                "frontline-news",
-                "war-room-rp",
-                "dispatch-rp",
-                "war-assets-rp",
-                "meta"
-            };
+        //private static async Task Discord_MessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs e)
+        //{
+        //    var command = new Commands();
+        //    string[] messageStrings = new string[] { "bot", "multibot" };
+        //    var strArray = e.Message.Content.Split(" ");
+        //    string[] prohibChannelArr = new string[]
+        //    {
+        //        "command-chat",
+        //        "officer-quarters",
+        //        "op-planning",
+        //        "frontline-news",
+        //        "war-room-rp",
+        //        "dispatch-rp",
+        //        "war-assets-rp",
+        //        "meta"
+        //    };
 
-            if ((e.Guild.Name == "MultiCorp" || e.Guild.Name == "Man vs Owlbear") && e.Author.Username != "MultiBot" && !prohibChannelArr.Contains(e.Channel.Name)) 
-            {
-                if (strArray.Intersect(messageStrings).Any() || e.MentionedUsers.Any(x => x.Username == "MultiBot"))
-                {
-                    await Task.Run(() => command.SkynetProtocol(e));
-                }
-            }
-            else
-            {
-                await Task.CompletedTask;
-            }       
-        }
+        //    if ((e.Guild.Name == "MultiCorp" || e.Guild.Name == "Man vs Owlbear") && e.Author.Username != "MultiBot" && !prohibChannelArr.Contains(e.Channel.Name)) 
+        //    {
+        //        if (strArray.Intersect(messageStrings).Any() || e.MentionedUsers.Any(x => x.Username == "MultiBot"))
+        //        {
+        //            await Task.Run(() => command.SkynetProtocol(e));
+        //        }
+        //    }
+        //    else
+        //    {
+        //        await Task.CompletedTask;
+        //    }       
+        //}
 
         static void CurrentDomain_ProcessExit(object sender, EventArgs e)
         {
